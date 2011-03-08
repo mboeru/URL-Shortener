ec228efcab438cb386f9435d05c7f65bb59b3ae6
mboeru <mboeru@gmail.com>
2011-03-08 22:20:18 +0200

    commit rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shortener::Application.config.secret_token = '35e0c3042535e8fced4141517339ff68740176fd3c509d12c115b34c2e6e514c4c6ef782e14ecf8f6c8308323230cb59439feb052ac16ef0cbbc7f7f7f16c1d2'
